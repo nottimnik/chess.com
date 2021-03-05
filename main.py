af3cb2b2b904359d77798a8020c7d7fb96ae9ef9
timnik <72356939+nottimnik@users.noreply.github.com>
2021-03-05 20:45:14 +0200

    Added stats command

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
+#Discord Libraries
 import discord
 from discord.ext import commands
+from discord.utils import get
+from discord.ext.commands import Bot
 
 client = commands.Bot(command_prefix="-")
+
+#Other Files
+from src.user_stats import get_player_rating
+
+#removes the default help command
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("----------")
+    await client.change_presence(activity=discord.Game(name="-help"))
+
+@client.command()
+async def help(ctx):
+    em = discord.Embed(title = "SOME1 Help Commands")
+    em.add_field(name = "!stats <username>", value = "`get the stats of the given user`")
+    await ctx.send(embed = em)
+
+@client.command() #Stats Command
+async def stats(ctx, username = None):
+    if(username == None):
+        em = discord.Embed(title="You need to enter the username of the user you want to get the stats of", description = "Usage: `!kick [username]`", color = 15158332)
+        await ctx.send(embed = em)
+    else:
+        em = discord.Embed(title = f"{username}\'s Stats", description = f"The chess.com stats of the user: {username}", color = 12370112)
+        ratings = get_player_rating(username)
+        em.add_field(name = f"Blitz", value = f"{ratings[0]}")
+        em.add_field(name = f"Rapid", value = f"{ratings[1]}")
+        em.add_field(name = f"Bullet", value = f"{ratings[2]}")
+        em.add_field(name = f"Daily", value = f"{ratings[3]}")
+        em.add_field(name = f"Puzzles", value = f"{ratings[4]}")
+        em.add_field(name = f"Puzzle Rush", value = f"{ratings[5]}")
+        await ctx.send(embed = em)
 
 ##reads the token from the file token.txt
 token = open(f"Token/token.txt", "r")
---
+++ src/user_stats.py
@@ -0,0 +1,19 @@
+from chessdotcom import get_player_stats
+import pprint
+
+printer = pprint.PrettyPrinter()
+
+ratings = [] #where all the current ratings of the player are stored
+
+def get_player_rating(username): #the function returns the current ratings of the player
+	data = get_player_stats(username).json
+	categories = ['chess_blitz', 'chess_rapid', 'chess_bullet', 'chess_daily']
+	for category in categories:
+		ratings.append(f'{data[category]["last"]["rating"]}')
+
+	ratings.append(f'{data["tactics"]["highest"]["rating"]}')
+	ratings.append(f'{data["puzzle_rush"]["best"]["score"]}')
+
+	return ratings
+
+	#Indexes of the ratings: 0 - blitz ; 1 - rapid ; 2 - bullet ; 3 - daily ; 5 - puzzle ; 6 - puzzle rush
